92a78ab90bddb8517c233595766ee9587f2b1f73
Vitor <vitordiasalves@gmail.com>
2026-03-12 09:32:54 -0300

    feat: Implementa algoritmos de busca em árvore binária: - Em ordem: percorre a lista da esquerda para a direita, iniciando da camada mais baixa da árvore - Pré ordem: percorre a lista de cima para baixo, priorizando o lado esquerdo que é percorrido antes do lado direito - Pós ordem: percorre a lista das camadas mais abaixo, priorizando o lado esquerdo que é percorrido antes do lado direito.

--- BuscarArvoreBinaria/main.c
+++ BuscarArvoreBinaria/main.c
@@ -19,16 +19,6 @@ No* criarNo(int valor)
     return novo;
 }
 
-void emOrdem(No* raiz)
-{
-    if (raiz != NULL)
-    {
-        emOrdem(raiz->esq);
-        printf("- %d -", raiz->valor);
-        emOrdem(raiz->dir);
-    }
-}
-
 No* inserirNo(No* raiz, int valor)
 {
     if (raiz == NULL)
@@ -52,47 +42,58 @@ No* inserirNo(No* raiz, int valor)
     return raiz;
 }
 
-int buscar(struct No* raiz, int valor)
+void emOrdem(struct  No* raiz){
+    if (raiz != NULL){
+        emOrdem(raiz->esq);
+        printf(" %d ", raiz->valor);
+        emOrdem(raiz->dir);
+    }
+}
+
+void preOrdem(struct No* raiz)
 {
-    if ( raiz == NULL)
-    {
-        return 0;
-    }
-
-    if ( raiz->valor == valor)
-    {
-        return 1;
-    }
-
-    if (valor < raiz-> valor)
-    {
-        return buscar(raiz->esq, valor);
+     if (raiz != NULL) {
+        printf(" %d ", raiz->valor);
+        preOrdem(raiz->esq);
+        preOrdem(raiz->dir);
     } 
-    else
-    {
-        return buscar(raiz->dir, valor);
+};
+
+void posOrdem(struct No* raiz){
+    if (raiz != NULL){
+        posOrdem(raiz->esq);
+        posOrdem(raiz->dir);
+        printf(" %d ", raiz->valor);
     }
+
 }
 
 int main(void)
 {
-    No* raiz = NULL;
+    No* raiz = criarNo(50);
 
-    raiz = inserirNo(raiz, 50);
-    raiz = inserirNo(raiz, 30);
-    raiz = inserirNo(raiz, 70);
-    raiz = inserirNo(raiz, 20);
+    raiz->esq = criarNo(30);
+    raiz->dir = criarNo(70);
+    
+    raiz->esq->esq = criarNo(20);
+    raiz->esq->dir = criarNo(40);
+    
+    raiz->dir->esq = criarNo(60);
+    raiz->dir->dir = criarNo(80);
 
     int valor = 20;
 
-    if(buscar(raiz, valor))
-    {
-        printf("Valor encontrado");
-    } else {
-        printf("Valor não encontrado");
-    }
-
+    printf("\nEM ORDEM:");
     emOrdem(raiz);    
+    printf("");
+
+    printf("\nPRE ORDEM:");
+    preOrdem(raiz);    
+    printf("");
+
+    printf("\nPOS ORDEM:");
+    posOrdem(raiz);    
+    printf("");
 
     return 0;
 }
